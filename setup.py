208dcccfd3d2a8b2bc9e7ff9c15d65b0df895aa2
rk1a <robertklassert@pm.me>
2022-09-13 10:30:28 +0200

    downgrade stable baselines 3 version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ PARALLEL_REQUIRE = ["ray[debug,tune]~=2.0.0"]
 PYTYPE = ["pytype==2022.7.26"] if IS_NOT_WINDOWS else []
 if IS_NOT_WINDOWS:
     # TODO(adam): use this for Windows as well once PyPI is at >=1.6.1
-    STABLE_BASELINES3 = "stable-baselines3>=1.6.0"
+    STABLE_BASELINES3 = "stable-baselines3>=1.4.0"
 else:
     STABLE_BASELINES3 = (
         "stable-baselines3@git+"
